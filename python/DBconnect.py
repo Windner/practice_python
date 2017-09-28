431a2c3f759051e3cd3a1252a64b81de094dfa9f
root <root@urm-campaign.htc.com.tw>
2017-09-28 17:42:18 +0800

    commit dbconnect

--- python/DBconnect.py
+++ python/DBconnect.py
@@ -1,6 +1,6 @@
 import MySQLdb
 
-class DBConnect:
+class DBConn:
     def __init__(self):
         self.user = 'root'
         self.host = '0.0.0.0'
@@ -9,28 +9,58 @@ class DBConnect:
         self.port = 6603
 
     def dbconnect(self):
-        self.db = MySQLdb.connect(self.host,self.user,self.password,self.dbname, self.port)
-        self.cursor = self.db.cursor()
+        try:
+            self.db = MySQLdb.connect(self.host,self.user,self.password,self.dbname, self.port)
+            self.cursor = self.db.cursor()
+
+        except MySQLdb.Error as e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
 
     def dbclose(self):
-        self.db.close()
+        try:
+            self.db.close()
+        
+        except MySQLdb.Error as e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
 
     def runQuery(self, sql):
-        self.cursor.execute(sql)
-        self.results = self.cursor.fetchall()
+        try:
+            self.cursor.execute(sql)
+            self.results = self.cursor.fetchall()
+
+        except MySQLdb.Error as e:
+            self.db.rollback()
+            print "Error %d: %s" % (e.args[0], e.args[1])
 
     def runInsert(self,sql):
-        self.cursor.execute(sql)
-        self.db.commit()
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except MySQLdb.Error as e:
+            self.db.rollback()
+            print "Error %d: %s" % (e.args[0], e.args[1])
 
     def runDelete(self, sql):
-        self.cursor.execute(sql)
-        self.db.commit()
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except MySQLdb.Error as e:
+            self.db.rollback()
+            print "Error %d: %s" % (e.args[0], e.args[1])
 
     def runUpdate(self, sql):
-        self.cursor.execute(sql)
-        self.db.commit()
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except MySQLdb.Error as e:
+            self.db.rollback()
+            print "Error %d: %s" % (e.args[0], e.args[1])
 
     def createTable(self, table_name, sql):
-        self.cursor.execute("DROP TABLE IF EXISTS %s" % table_name)
-        self.cursor.execute(sql)
+        try:
+            self.cursor.execute("DROP TABLE IF EXISTS %s" % table_name)
+            self.cursor.execute(sql)
+
+        except MySQLdb.Error as e:
+            self.db.rollback()
+            print "Error %d: %s" % (e.args[0], e.args[1])
--- python/query.py
+++ python/query.py
@@ -1,14 +1,28 @@
-from DBconnect import DBConnect
+from DBconnect import DBConn
 
 if __name__ == "__main__":
-    try:
-        DBConnect.dbconnect()
-        # create table
-        createTqbleSQL = """CREATE TABLE EMPLOYEE (
-         FIRST_NAME  CHAR(20) NOT NULL,
-         LAST_NAME  CHAR(20),
-         AGE INT,  
-         SEX CHAR(1),
-         INCOME FLOAT )"""
-        DBConnect.dbclose()
+    
+    db_use = DBConn()
+    db_use.dbconnect()
+    # create table
+    createTqbleSQL = """CREATE TABLE EMPLOYEE (
+        FIRST_NAME  CHAR(20) NOT NULL,
+        LAST_NAME  CHAR(20),
+        AGE INT,  
+        SEX CHAR(1),
+        INCOME FLOAT )"""
+    db_use.createTable("EMPLOYEE", createTqbleSQL)
+
+    InsertSQL = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+
+    db_use.runInsert(InsertSQL)
+
+    UpdateSQL = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+
+    db_use.runUpdate(UpdateSQL)
+    
+    db_use.dbclose()
         
+    
